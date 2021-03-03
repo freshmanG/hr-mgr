bbabc1aa8724698a6f1ba20b76bf9e1d213a69bd
guojing <673866357@qq.com>
2021-03-03 17:17:40 +0800

    完成前端登录注册相关界面

---
+++ hr-mgr-fe/src/views/Auth/index.js
@@ -0,0 +1,15 @@
+
+import { defineComponent } from 'vue';
+
+import { UserOutlined,UnlockOutlined,QrcodeOutlined } from '@ant-design/icons-vue';
+
+export default defineComponent({
+    components: {
+        UserOutlined,
+        UnlockOutlined,
+        QrcodeOutlined,
+    },
+    setup() {
+        
+    },
+});
