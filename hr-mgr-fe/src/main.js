95744c9c69b2257e88f7fad827d3da57b5c87161
guojing <673866357@qq.com>
2021-03-21 19:10:04 +0800

    前端管理界面

--- hr-mgr-fe/src/main.js
+++ hr-mgr-fe/src/main.js
@@ -4,9 +4,12 @@ import router from './router';
 import store from './store';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import SpaceBetween from './components/SpaceBetween/index.vue'
+import { SHOW_ALL } from 'ant-design-vue/lib/vc-tree-select';
 
 createApp(App)
     .use(store)
     .use(router)
     .use(Antd)
+    .component('space-between',SpaceBetween)
     .mount('#app');
